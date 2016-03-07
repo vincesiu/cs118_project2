009fa4cd6566b9edbac4d8107498050fd41ab9f2
Julien Brundrett <julien.brundret@gmail.com>
2016-03-07 04:01:54 -0800

    fix some stuff

--- sender.c
+++ sender.c
@@ -1,7 +1,7 @@
 /* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
-   process for each connection
+    The port number is passed as an argument 
+    This version runs forever, forking off a separate 
+    process for each connection
 */
 #include <stdio.h>
 #include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
@@ -24,48 +24,50 @@ void err(char *msg)
 
 int main(int argc, char *argv[])
 {
-     //int len = 200;
-   	 //char *buffer = malloc(sizeof(char) * (len + 1));
-     int len = 255;
-     char buffer[256];
+    //int len = 200;
+     //char *buffer = malloc(sizeof(char) * (len + 1));
+    int len = 255;
+    char buffer[256];
+    char filename[256];
+    FILE* fp = NULL;
 
+    if (argc < 2) 
+    err("no port provided");
 
-     if (argc < 2) 
-         err("no port provided");
+    socket_info_st *s = init_socket(atoi(argv[1]), 0, 1);
 
-     socket_info_st *s = init_socket(atoi(argv[1]), 0, 1);
-     
-     socket_recv(s, buffer, len);
-     FILE* fp = fopen(buffer,"r")
+    // socket_recv(s, buffer, len);
+    // FILE* fp = fopen(buffer,"r");
 
-     while (fp == NULL)
-     {
-        buffer = "ERROR: File not found.\n";
-        socket_send(s, buffer, strlen(buffer));
-
-        memset(buffer, 0, len);
+    while (fp == NULL)
+    {
         socket_recv(s, buffer, len);
-        FILE* fp = fopen(buffer,"r")
-     }
+        sscanf(buffer, "%s", filename);
+        fp = fopen(filename,"r");
+        if (fp == NULL) {
+            strcpy(buffer, "ERROR: File not found.\n");
+            socket_send(s, buffer, strlen(buffer));
+        }
+    }
 
-   	 printf("Sucessfully opened the requested file: %s\n", buffer);
+    printf("Sucessfully opened the requested file: %s\n", buffer);
 
-     // memset(buffer, 0, len);
-     // buffer[0] = 'd';
-     // buffer[1] = 'a';
-     // buffer[2] = 'a';
-     // buffer[3] = 'g';
-     // socket_send(s, buffer, strlen(buffer));
+    // memset(buffer, 0, len);
+    // buffer[0] = 'd';
+    // buffer[1] = 'a';
+    // buffer[2] = 'a';
+    // buffer[3] = 'g';
+    // socket_send(s, buffer, strlen(buffer));
 
 
-     
-     // socket_recv(s, buffer, len);
-   	 // printf("Here is my ack: %s\n",buffer);
 
-     
+    // socket_recv(s, buffer, len);
+    // printf("Here is my ack: %s\n",buffer);
 
-     free_socket(s); 
-         
-     return 0; 
+
+
+    free_socket(s); 
+       
+    return 0; 
 }
 
