60c156a4e1bff009088b6ba939964ef40c8c6dd8
Julien Brundrett <julien.brundret@gmail.com>
2016-03-06 21:42:21 -0800

    structs changes

--- structs.h
+++ structs.h
@@ -1,8 +1,15 @@
 #include <sys/time.h>
+#define PACKET_SIZE 1000
+#define WINDOW_SIZE 5000
+#define MAX_SEQ_NO  30000
+#define TIMEOUT     3000   // in ms
 
 typedef struct frame_node {
-    char* data;
-    int id;
-    int ack;
+    char* data; // header + data, cannot exceed PACKET_SIZE
+    int len; // length of JUST DATA
+    int seq_no; // in bytes, cannot exceed MAX_SEQ_NO
+    int ack;  // 0 if ack not yet sent/received, 1 if ack sent/received
+    int corrupt; // 0 if normal, 1 if corrupt
+    int lost; // 0 if normal, 1 if lost
     struct timeval timeSent;    
 } Frame;
